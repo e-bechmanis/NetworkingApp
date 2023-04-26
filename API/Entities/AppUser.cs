1b17a9f0d680bbb6522319e72a8587d7396c5eef
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-04-25 23:31:41 -0400

    added DbContext / entity framework

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        // If nullable was enabled, as of .NET6 string values are by default required
+        // string? for optional value
+        public string UserName { get; set; }
+    }
+}
