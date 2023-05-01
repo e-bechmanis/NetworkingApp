e755b1cf4b663a9555398680c829f18abefc62b8
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-05-01 12:25:28 -0400

    added validation

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        //Both username and password props are required for registration
+        //Validation on RegisterDTO level
+        [Required]
         public string Username {get; set;}
+        
+        [Required]
         public string Password {get; set;}
     }
 }
