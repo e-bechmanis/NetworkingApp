a60e9c5caefc288873d14cd59fe7bea09c5aa736
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-05-01 12:26:17 -0400

    included token service

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+// AddTransient is too short-lived for HTTP request
+// AddSingleton <-- token will reside in-memory for the entire length of app lifetime. Caching service would be a use-case
+// Recommended to add interfaces, because it's easier to test against interfaces
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
