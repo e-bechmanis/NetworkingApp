6dc2749f3bfbf13039aa6f73b86e2958e2e422e6
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-05-01 20:46:04 -0400

    added account service login functionality

--- ui/src/app/_services/account.service.ts
+++ ui/src/app/_services/account.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +9,31 @@ import { Injectable } from '@angular/core';
 export class AccountService {
 
   baseUrl = "https://localhost:5001/api/"
+  // User | null is a union type, where a thing can be of 1 or more types
+  private CurrentUserSource = new BehaviorSubject<User | null>(null);
+  // it is a convention to user $ to signify an Observable
+  currentUser$ = this.CurrentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
   login(model: any){
-    return this.http.post(this.baseUrl + 'account/login', model);
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user))
+          this.CurrentUserSource.next(user);
+        }
+      }
+    ))
+  }
+
+  setCurrentUser(user: User){
+    this.CurrentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.CurrentUserSource.next(null);
   }
 }
