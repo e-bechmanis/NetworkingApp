4bd429214869ecf9a736d264b0cb4e0e150823c7
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-05-03 12:20:29 -0400

    connected with api to display mentors

--- ui/src/app/mentors/mentor-detail/mentor-detail.component.ts
+++ ui/src/app/mentors/mentor-detail/mentor-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Mentor } from 'src/app/_models/mentor';
+import { MentorsService } from 'src/app/_services/mentors.service';
 
 @Component({
   selector: 'app-mentor-detail',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mentor-detail.component.css']
 })
 export class MentorDetailComponent implements OnInit {
+  mentor: Mentor | undefined;
 
-  constructor() { }
+  // ActivateRoute to access username in /mentors/:username route
+  constructor(private mentorService: MentorsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMentor();
+  }
+
+  loadMentor(){
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.mentorService.getMentor(username).subscribe({
+      next: mentor => this.mentor = mentor
+    })
   }
 
 }
--- ui/src/app/mentors/mentor-list/mentor-list.component.ts
+++ ui/src/app/mentors/mentor-list/mentor-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Mentor } from 'src/app/_models/mentor';
+import { MentorsService } from 'src/app/_services/mentors.service';
 
 @Component({
   selector: 'app-mentor-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mentor-list.component.css']
 })
 export class MentorListComponent implements OnInit {
+  mentors: Mentor[] = []
 
-  constructor() { }
+  constructor(private mentorService: MentorsService) { }
 
   ngOnInit(): void {
+    this.loadMentors();
+  }
+
+  loadMentors(){
+    this.mentorService.getMentors().subscribe({
+      next: mentors => this.mentors = mentors
+    })
   }
 
 }
