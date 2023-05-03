ed13ada55a895a2832aa2ae943f7b5867c5e0cec
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-05-02 23:26:15 -0400

    added error handling

--- ui/src/app/nav/nav.component.ts
+++ ui/src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-nav',
@@ -11,15 +13,15 @@ export class NavComponent implements OnInit {
   model: any = {}
 
   // making it public, so that account service can be accessed in the template
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: () => this.router.navigateByUrl('/mentors'),
-      error: err => console.log(err)
+      next: () => this.router.navigateByUrl('/mentors')
     })
   }
 
