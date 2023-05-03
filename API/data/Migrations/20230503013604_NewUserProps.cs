f256e592c672181339b65203f38c1a80957ea689
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-05-02 23:31:12 -0400

    new db migration

---
+++ API/data/Migrations/20230503013604_NewUserProps.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewUserProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stack",
+                table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "Stack",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    CanMentor = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Learning = table.Column<bool>(type: "INTEGER", nullable: false),
+                    AppUserId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stack", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stack_Users_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stack_AppUserId",
+                table: "Stack",
+                column: "AppUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stack");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Stack",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
