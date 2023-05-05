f0c226eaaea2676eff84650ddaf1d369bc57bcc0
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-05-05 10:30:40 -0400

    updated to use ng reactive forms

--- ui/src/app/register/register.component.ts
+++ ui/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,23 +12,50 @@ export class RegisterComponent implements OnInit {
   // child to parent communication
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
+  registerForm: FormGroup = new FormGroup({});
+  fieldTextType: boolean = false;
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, 
+          Validators.minLength(4), Validators.maxLength(8)]),
+      controlPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    })
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  // returning Validator function
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
   register(){
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel();
-      },
-      error: err => this.toastr.error(err.error)
-    })
+    console.log(this.registerForm?.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancel();
+    //   },
+    //   error: err => this.toastr.error(err.error)
+    // })
   }
 
   cancel(){
     this.cancelRegister.emit(false);
+  }
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
   }
 
 }
